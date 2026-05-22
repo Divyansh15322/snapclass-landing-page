deb3b8e0786c141fd27c96d5461d2d4db34c3257
Divyansh15322 <idivyansh88@gmail.com>
2026-05-23 02:03:49 +0530

    Updated images

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+app.config['TEMPLATES_AUTO_RELOAD'] = True
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5002)
+    app.run(debug=True, use_reloader=True, port=5003)
